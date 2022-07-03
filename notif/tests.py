e11524a4ee89ab5abfb8042c96fead8cf37fa933
NigerianCoder <gbemilanre@gmail.com>
2022-07-03 14:41:10 +0100

    CGSmsClient functionality implementation completed

--- notif/tests.py
+++ notif/tests.py
@@ -7,6 +7,14 @@ from django.test import TestCase
 
 # arr = list(map(int, input().rstrip().split()))
 
-A = [1, 2, 3, 4]
-for i in (range(len(A))):
-    print(A[i], end="")
+# A = [1, 2, 3, 4]
+# for i in reversed(range(len(A))):
+#     print(A[i], end="")
+n = input()    
+phone_book = {"sam": 99912222, "tom": 11122222, "harry": 12299933}
+
+for value in phone_book:
+    if value in n:
+        print(value,'=', phone_book[value])
+    else:
+        print("Not found")
