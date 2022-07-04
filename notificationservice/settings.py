21973025f6873342e5bf0d6579104d1dede5150d
NigerianCoder <gbemilanre@gmail.com>
2022-07-04 12:11:06 +0100

    emailer implementation completed

--- notificationservice/settings.py
+++ notificationservice/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
     'notif',
     'rest_framework',
-    'channels'
+    
 ]
 
 MIDDLEWARE = [
